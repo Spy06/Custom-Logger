d02cb50974de6bc911f049c65995da4ab0cb5476
Spy <89559710+Spy06@users.noreply.github.com>
2021-12-23 20:08:21 +0700

    Add files via upload

---
+++ Logger.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace CustomLogger
+{
+    public static class Logger
+    {
+        public static void LogMessage (string message, LoggingType logType){
+            switch (logType){
+                case LoggingType.Normal:
+                    Normal(message);
+                    break;
+                case LoggingType.Warning:
+                    Warning (message);
+                    break;
+                case LoggingType.Error:
+                    Error (message);
+                    break;
+            }
+        }
+
+        private static void Normal (string msg){
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine (msg);
+        }
+
+        private static void Warning (string msg){
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine ("Warning" + msg);
+        }
+
+        private static void Error (string msg){
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine ("Error" + msg);
+        }
+    }
+
+    public enum LoggingType {
+        Normal, Warning, Error
+    }
+}
